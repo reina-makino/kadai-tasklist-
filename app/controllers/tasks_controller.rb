b9c2f40aa9c34799a1eda1a279dd9a58f00678bb
makino.makino <doremisora3@gmail.com>
2019-03-25 06:22:59 +0000

    投稿機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   
@@ -15,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -60,8 +62,13 @@ class TasksController < ApplicationController
   end   
   
   def task_params
-    params.require(:task).permit(:content   , :status)
+    params.require(:task).permit(:content, :status)
   end
   
-  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
