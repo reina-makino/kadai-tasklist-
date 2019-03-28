30102c90226e8b8f1a1e1527c0b93eefd481a34c
makino.makino <doremisora3@gmail.com>
2019-03-28 09:11:22 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy :create :index]
+  before_action :correct_user
 
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
