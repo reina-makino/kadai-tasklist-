700e2d7a93650fc31167d3d88d4b0790dcd2601c
makino.makino <doremisora3@gmail.com>
2019-03-28 09:44:36 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   
   def index
     
-      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
   end
 
   def show
